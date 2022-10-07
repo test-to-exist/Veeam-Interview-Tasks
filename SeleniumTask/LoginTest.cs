c40737a25038063ade04b39713811aab0fa6006c
witty.koala3176@fastmail.com <9LRp9KmqeXMan.Hu3t_H8ApssjzZ!iwZ>
2022-10-07 11:18:53 +0200

    Initial Commit

---
+++ SeleniumTask/LoginTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SeleniumTask
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
